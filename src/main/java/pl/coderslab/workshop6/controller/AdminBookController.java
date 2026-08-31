47188e4083f8b9fa4c8e25ec4bb3a52b00945774
pmalek299 <pmalek@visa.com>
2026-08-31 21:24:48 +0200

    public api

--- src/main/java/pl/coderslab/workshop6/controller/AdminBookController.java
+++ src/main/java/pl/coderslab/workshop6/controller/AdminBookController.java
@@ -1,4 +1,23 @@
 package pl.coderslab.workshop6.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.coderslab.workshop6.service.BookService;
+
+@Controller
 public class AdminBookController {
+
+    private final BookService bookService;
+
+    public AdminBookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/admin/books")
+    public String books(Model model){
+        model.addAttribute("books", bookService.getBooks());
+
+        return "books/list";
+    }
 }
